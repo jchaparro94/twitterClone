22533d9c5918986069bd5375cd5779153db8a5be
Jose <chaparro.jjr@gmail.com>
2020-04-12 15:00:29 -0700

    first API endpoint created and receiving twitter data

--- api/app.js
+++ api/app.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-   res.send("Hello World");
+app.get('/tweets', (req, res) => {
+   const query = req.query.q;
+   const count = req.query.count;
+
+   const url = "https://api.twitter.com/1.1/search/tweets.json";
+
+   axios.get(url, {
+      params: {
+         q: query,
+         count: count
+      },
+      headers: {
+         "Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAAM%2FEDAEAAAAAz9nK9zf0VqspxKWhWcXyMLQ0LEI%3DlnmjuQm4f08UF50EqQ5sC67KiqMa2dBR7fnbMYFSnvWO4iKKe9"
+      }
+   }).then((response) => {
+      res.status(200).send(response.data);
+   }).catch((error) => {
+      res.status(400).send(error);
+   })
 });
 
 app.listen(PORT, () => console.log(`Twitter API listening on port ${PORT}`));
