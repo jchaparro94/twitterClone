6fcf42c570dd18a859621a2cb778a59231861467
Jose <chaparro.jjr@gmail.com>
2020-04-12 17:59:49 -0700

    tweet data now shows the text and photos on the app side

--- api/app.js
+++ api/app.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const app = express();
-const axios = require('axios');
+const Twitter = require('./api/helpers/twitter');
+const twitter = new Twitter;
 const PORT = 3000;
+require('dotenv').config();
+
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   next();
+});
 
 app.get('/tweets', (req, res) => {
    const query = req.query.q;
    const count = req.query.count;
-
-   const url = "https://api.twitter.com/1.1/search/tweets.json";
-
-   axios.get(url, {
-      params: {
-         q: query,
-         count: count
-      },
-      headers: {
-         "Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAAM%2FEDAEAAAAAz9nK9zf0VqspxKWhWcXyMLQ0LEI%3DlnmjuQm4f08UF50EqQ5sC67KiqMa2dBR7fnbMYFSnvWO4iKKe9"
-      }
-   }).then((response) => {
+   twitter.get(query, count).then((response) => {
       res.status(200).send(response.data);
    }).catch((error) => {
       res.status(400).send(error);
