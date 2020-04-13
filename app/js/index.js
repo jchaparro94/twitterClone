3a0df60e54f2935b3c80d4f3c6f85ac0040cd060
Jose <chaparro.jjr@gmail.com>
2020-04-13 16:22:53 -0700

    added HTML/CSS related to pictures and gifs for tweets

---
+++ app/js/index.js
@@ -0,0 +1,90 @@
+const URL = "http://localhost:3000/tweets";
+
+const onEnter = (e) => {
+   if ( e.key == "Enter") {
+      getTwitterData();
+   }
+};
+
+const getTwitterData = () => {
+   const query = document.getElementById('user-input-search').value;
+   if (!query) return;
+   const encodedQuery = encodeURIComponent(query);
+   const fullUrl = `${URL}?q=${encodedQuery}&count=10`;
+   fetch(fullUrl).then((response) => {
+      return response.json();
+   }).then((data) => {
+      buildTweets(data.statuses);
+   })
+}
+
+const buildTweets = (tweets, nextPage) => {
+   let twitterContent = '';
+   tweets.map((tweet) => {
+      twitterContent += `
+         <div class="tweet-container">
+            <div class="tweet-user-info">
+               <div class="tweet-user-profile">
+               
+               </div>
+               <div class="tweet-user-name-container">
+                  <div class="tweet-user-fullname">Jose Chaparro</div>
+                  <div class="tweet-user-username">@jchapa</div>
+               </div>
+            </div>
+         `
+         if (tweet.extended_entities && tweet.extended_entities.media.length > 0) {
+            twitterContent += buildImages(tweet.extended_entities.media);
+            twitterContent += buildVideo(tweet.extended_entities.media);
+         }        
+         twitterContent += `
+            <div class="tweet-text-container">
+               ${tweet.full_text}
+            </div>
+            <div class="tweet-date-container">
+               20 hours ago
+            </div>
+         </div>
+      `
+   })
+   document.querySelector('.tweets-list').innerHTML = twitterContent;
+};
+
+const buildImages = (mediaList) => {
+   let imagesContent = `<div class="tweet-image-container">`;
+   let imageExists = false;
+   mediaList.map((media) => {
+      console.log(media);
+      if (media.type == "photo") {
+         imageExists = true;
+         imagesContent += `<div class="tweet-image" style="background-image: url(${media.media_url_https})"></div>`
+      }
+   });
+   imagesContent += `</div>`;
+   return imageExists ? imagesContent : '';
+};
+
+const buildVideo = (mediaList) => {
+   let videoContent = `<div class="tweet-video-container">`;
+   let videoExists = false;
+   mediaList.map((media) => {
+      console.log(media);
+      if (media.type == "video") {
+         videoExists = true;
+         videoContent += `
+         <video controls>
+            <source src="${media.video_info.variants[0].url}" type="video/mp4">
+         </video>
+         `
+      } else if (media.type == "animated_gif") {
+         videoExists = true;
+         videoContent += `
+         <video loop autoplay>
+            <source src="${media.video_info.variants[0].url}" type="video/mp4">
+         </video>
+         `
+      }
+   });
+   videoContent += `</div>`;
+   return videoExists ? videoContent : '';
+};
